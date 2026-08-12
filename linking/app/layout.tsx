57aa77ea636f441e48f2445bbde3628943689f49
jeongjookim <jeongjoo@coconutsilo.com>
2026-08-12 11:05:39 +0900

    feat: ga4 태그 추가

--- linking/app/layout.tsx
+++ linking/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,22 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TN9DGLHWVJ"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TN9DGLHWVJ');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
